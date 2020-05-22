808a0f98560106b40a75934ce723d062e6cd87d2
NgLoader <nilsgereke1@gmail.com>
2020-05-23 00:47:00 +0200

    feat: added permission check

--- orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/util/PermissionUtil.java
+++ orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/util/PermissionUtil.java
@@ -5,6 +5,6 @@ import org.bukkit.entity.Player;
 public class PermissionUtil {
 
 	public static boolean canDeobfuscate(Player player) {
-		return false;/* TODO player.isOp() || player.hasPermission("orebfuscator.bypass");*/
+		return player.isOp() || player.hasPermission("orebfuscator.bypass");
 	}
 }
