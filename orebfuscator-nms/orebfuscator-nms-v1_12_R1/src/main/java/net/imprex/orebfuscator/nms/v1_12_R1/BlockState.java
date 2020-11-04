c4cc1d635b92e6da39fb13c4647899b7f78c0732
Maurice Eisenblätter <code@ingrim4.me>
2020-11-04 01:31:00 +0100

    chore: rollback breaking changes

--- orebfuscator-nms/orebfuscator-nms-v1_12_R1/src/main/java/net/imprex/orebfuscator/nms/v1_12_R1/BlockState.java
+++ orebfuscator-nms/orebfuscator-nms-v1_12_R1/src/main/java/net/imprex/orebfuscator/nms/v1_12_R1/BlockState.java
@@ -4,9 +4,8 @@ import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 
 import net.imprex.orebfuscator.nms.AbstractBlockState;
+import net.minecraft.server.v1_12_R1.BlockPosition;
 import net.minecraft.server.v1_12_R1.IBlockData;
-import net.minecraft.server.v1_12_R1.PlayerChunk;
-import net.minecraft.server.v1_12_R1.PlayerChunkMap;
 import net.minecraft.server.v1_12_R1.WorldServer;
 
 public class BlockState extends AbstractBlockState<IBlockData> {
@@ -23,10 +22,6 @@ public class BlockState extends AbstractBlockState<IBlockData> {
 	@Override
 	public void notifyBlockChange() {
 		WorldServer worldServer = ((CraftWorld) this.world).getHandle();
-		PlayerChunkMap chunkMap = worldServer.getPlayerChunkMap();
-		PlayerChunk playerChunk = chunkMap.getChunk(this.x >> 4, this.z >> 4);
-		if (playerChunk != null) {
-			playerChunk.a(this.x & 15, this.y & 15, this.z & 15);
-		}
+		worldServer.notify(new BlockPosition(this.x, this.y, this.z), this.state, this.state, 0);
 	}
 }
--- orebfuscator-nms/orebfuscator-nms-v1_9_R2/src/main/java/net/imprex/orebfuscator/nms/v1_9_R2/BlockState.java
+++ orebfuscator-nms/orebfuscator-nms-v1_9_R2/src/main/java/net/imprex/orebfuscator/nms/v1_9_R2/BlockState.java
@@ -4,9 +4,8 @@ import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_9_R2.CraftWorld;
 
 import net.imprex.orebfuscator.nms.AbstractBlockState;
+import net.minecraft.server.v1_9_R2.BlockPosition;
 import net.minecraft.server.v1_9_R2.IBlockData;
-import net.minecraft.server.v1_9_R2.PlayerChunk;
-import net.minecraft.server.v1_9_R2.PlayerChunkMap;
 import net.minecraft.server.v1_9_R2.WorldServer;
 
 public class BlockState extends AbstractBlockState<IBlockData> {
@@ -23,10 +22,6 @@ public class BlockState extends AbstractBlockState<IBlockData> {
 	@Override
 	public void notifyBlockChange() {
 		WorldServer worldServer = ((CraftWorld) this.world).getHandle();
-		PlayerChunkMap chunkMap = worldServer.getPlayerChunkMap();
-		PlayerChunk playerChunk = chunkMap.getChunk(this.x >> 4, this.z >> 4);
-		if (playerChunk != null) {
-			playerChunk.a(this.x & 15, this.y & 15, this.z & 15);
-		}
+		worldServer.notify(new BlockPosition(this.x, this.y, this.z), this.state, this.state, 0);
 	}
 }
