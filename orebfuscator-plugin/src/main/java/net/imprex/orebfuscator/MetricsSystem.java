c384a031f0b45251e2e04b686ba01fcfd41011b7
Maurice Eisenblätter <code@ingrim4.me>
2021-08-18 22:12:43 +0200

    chore: tidy up plugin.yml chore: change memory grouping for bstats

--- orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/MetricsSystem.java
+++ orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/MetricsSystem.java
@@ -1,11 +1,9 @@
 package net.imprex.orebfuscator;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.bstats.bukkit.Metrics;
 
 import net.imprex.orebfuscator.config.OrebfuscatorConfig;
+import net.imprex.orebfuscator.util.MathUtil;
 
 public class MetricsSystem {
 
@@ -18,22 +16,9 @@ public class MetricsSystem {
 	}
 
 	public void addMemoryChart() {
-		this.metrics.addCustomChart(new Metrics.DrilldownPie("system_memory", () -> {
-			final Map<String, Map<String, Integer>> result = new HashMap<>();
-			final Map<String, Integer> exact = new HashMap<>();
-
-			long memory = Runtime.getRuntime().maxMemory();
-			if (memory == Long.MAX_VALUE) {
-				exact.put("unbound", 1);
-				result.put("unbound", exact);
-			} else {
-				long megaByte = (long) Math.ceil(memory / 1048576d);
-				long gigaByte = (long) Math.ceil(memory / 1073741824d);
-				exact.put(megaByte + "MiB", 1);
-				result.put(gigaByte + "GiB", exact);
-			}
-
-			return result;
+		this.metrics.addCustomChart(new Metrics.SimplePie("systemMemory", () -> {
+			int memory = (int) (Runtime.getRuntime().maxMemory() / 1073741824L);
+			return MathUtil.ceilToPowerOfTwo(memory) + "GiB";
 		}));
 	}
 
