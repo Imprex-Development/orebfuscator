cf10d707b5cb72e5f3bcebc2a9676817b752eaf6
Maurice Eisenblätter <code@ingrim4.me>
2023-09-09 23:45:21 +0200

    chore: wip - cleanup nms code

--- orebfuscator-nms/orebfuscator-nms-api/src/main/java/net/imprex/orebfuscator/OrebfuscatorNms.java
+++ orebfuscator-nms/orebfuscator-nms-api/src/main/java/net/imprex/orebfuscator/OrebfuscatorNms.java
@@ -3,12 +3,10 @@ package net.imprex.orebfuscator;
 import java.lang.reflect.Constructor;
 
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 import net.imprex.orebfuscator.config.Config;
 import net.imprex.orebfuscator.nms.AbstractRegionFileCache;
-import net.imprex.orebfuscator.nms.BlockStateHolder;
 import net.imprex.orebfuscator.nms.NmsManager;
 import net.imprex.orebfuscator.nms.ReadOnlyChunk;
 import net.imprex.orebfuscator.util.BlockPos;
@@ -80,12 +78,16 @@ public class OrebfuscatorNms {
 		return instance.getReadOnlyChunk(world, chunkX, chunkZ);
 	}
 
-	public static BlockStateHolder getBlockState(World world, Block block) {
-		return instance.getBlockState(world, block.getX(), block.getY(), block.getZ());
+	public static int getBlockState(World world, BlockPos position) {
+		return getBlockState(world, position.x, position.y, position.z);
 	}
 
-	public static BlockStateHolder getBlockState(World world, int x, int y, int z) {
+	public static int getBlockState(World world, int x, int y, int z) {
 		return instance.getBlockState(world, x, y, z);
+	}
+
+	public static void sendBlockUpdates(World world, Iterable<BlockPos> iterable) {
+		instance.sendBlockUpdates(world, iterable);
 	}
 
 	public static void sendBlockUpdates(Player player, Iterable<BlockPos> iterable) {
--- orebfuscator-nms/orebfuscator-nms-api/src/main/java/net/imprex/orebfuscator/nms/NmsManager.java
+++ orebfuscator-nms/orebfuscator-nms-api/src/main/java/net/imprex/orebfuscator/nms/NmsManager.java
@@ -25,7 +25,9 @@ public interface NmsManager {
 
 	ReadOnlyChunk getReadOnlyChunk(World world, int chunkX, int chunkZ);
 
-	BlockStateHolder getBlockState(World world, int x, int y, int z);
+	int getBlockState(World world, int x, int y, int z);
+
+	void sendBlockUpdates(World world, Iterable<BlockPos> iterable);
 
 	void sendBlockUpdates(Player player, Iterable<BlockPos> iterable);
 
