eb01632a1aa9617820a5bc782b0ca708c12d5389
Maurice Eisenblätter <code@ingrim4.me>
2023-09-09 20:36:39 +0200

    chore: deduplicate compatibility code

--- orebfuscator-compatibility/orebfuscator-compatibility-folia/src/main/java/net/imprex/orebfuscator/compatibility/folia/FoliaCompatibilityLayer.java
+++ orebfuscator-compatibility/orebfuscator-compatibility-folia/src/main/java/net/imprex/orebfuscator/compatibility/folia/FoliaCompatibilityLayer.java
@@ -1,19 +1,12 @@
 package net.imprex.orebfuscator.compatibility.folia;
 
-import java.util.concurrent.CompletableFuture;
-
-import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 
-import net.imprex.orebfuscator.OrebfuscatorNms;
-import net.imprex.orebfuscator.compatibility.CompatibilityLayer;
 import net.imprex.orebfuscator.compatibility.CompatibilityScheduler;
+import net.imprex.orebfuscator.compatibility.paper.AbstractPaperCompatibilityLayer;
 import net.imprex.orebfuscator.config.Config;
-import net.imprex.orebfuscator.nms.ReadOnlyChunk;
-import net.imprex.orebfuscator.util.ChunkDirection;
-import net.imprex.orebfuscator.util.ChunkPosition;
 
-public class FoliaCompatibilityLayer implements CompatibilityLayer {
+public class FoliaCompatibilityLayer extends AbstractPaperCompatibilityLayer {
 
 	private final FoliaScheduler scheduler;
 
@@ -25,22 +18,4 @@ public class FoliaCompatibilityLayer implements CompatibilityLayer {
 	public CompatibilityScheduler getScheduler() {
 		return this.scheduler;
 	}
-
-	@Override
-	public CompletableFuture<ReadOnlyChunk[]> getNeighboringChunks(World world, ChunkPosition position) {
-		CompletableFuture<?>[] futures = new CompletableFuture<?>[4];
-		ReadOnlyChunk[] neighboringChunks = new ReadOnlyChunk[4];
-
-		for (ChunkDirection direction : ChunkDirection.values()) {
-			int chunkX = position.x + direction.getOffsetX();
-			int chunkZ = position.z + direction.getOffsetZ();
-			int index = direction.ordinal();
-
-			futures[index] = world.getChunkAtAsync(chunkX, chunkZ).thenAccept(chunk -> {
-				neighboringChunks[index] = OrebfuscatorNms.getReadOnlyChunk(world, chunkX, chunkZ);
-			});
-		}
-
-		return CompletableFuture.allOf(futures).thenApply(v -> neighboringChunks);
-	}	
 }
--- orebfuscator-compatibility/orebfuscator-compatibility-paper/src/main/java/net/imprex/orebfuscator/compatibility/paper/PaperCompatibilityLayer.java
+++ orebfuscator-compatibility/orebfuscator-compatibility-paper/src/main/java/net/imprex/orebfuscator/compatibility/paper/PaperCompatibilityLayer.java
@@ -1,46 +1,21 @@
 package net.imprex.orebfuscator.compatibility.paper;
 
-import java.util.concurrent.CompletableFuture;
-
-import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
 
-import net.imprex.orebfuscator.OrebfuscatorNms;
-import net.imprex.orebfuscator.compatibility.CompatibilityLayer;
 import net.imprex.orebfuscator.compatibility.CompatibilityScheduler;
+import net.imprex.orebfuscator.compatibility.bukkit.BukkitScheduler;
 import net.imprex.orebfuscator.config.Config;
-import net.imprex.orebfuscator.nms.ReadOnlyChunk;
-import net.imprex.orebfuscator.util.ChunkDirection;
-import net.imprex.orebfuscator.util.ChunkPosition;
 
-public class PaperCompatibilityLayer implements CompatibilityLayer {
+public class PaperCompatibilityLayer extends AbstractPaperCompatibilityLayer {
 
-	private final PaperScheduler scheduler;
+	private final BukkitScheduler scheduler;
 
 	public PaperCompatibilityLayer(Plugin plugin, Config config) {
-		this.scheduler = new PaperScheduler(plugin);
+		this.scheduler = new BukkitScheduler(plugin);
 	}
 
 	@Override
 	public CompatibilityScheduler getScheduler() {
 		return this.scheduler;
 	}
-
-	@Override
-	public CompletableFuture<ReadOnlyChunk[]> getNeighboringChunks(World world, ChunkPosition position) {
-		CompletableFuture<?>[] futures = new CompletableFuture<?>[4];
-		ReadOnlyChunk[] neighboringChunks = new ReadOnlyChunk[4];
-
-		for (ChunkDirection direction : ChunkDirection.values()) {
-			int chunkX = position.x + direction.getOffsetX();
-			int chunkZ = position.z + direction.getOffsetZ();
-			int index = direction.ordinal();
-
-			futures[index] = world.getChunkAtAsync(chunkX, chunkZ).thenAccept(chunk -> {
-				neighboringChunks[index] = OrebfuscatorNms.getReadOnlyChunk(world, chunkX, chunkZ);
-			});
-		}
-
-		return CompletableFuture.allOf(futures).thenApply(v -> neighboringChunks);
-	}	
 }
