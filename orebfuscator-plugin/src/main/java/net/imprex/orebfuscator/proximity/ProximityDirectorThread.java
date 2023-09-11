c931883105397954d0a3071512bd64b6bd89abd3
kFor <kforbro@gmail.com>
2023-09-12 02:25:51 +0300

    event priorities changed to `MONITOR` and enabled ignoring canceled events

--- orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/proximity/ProximityDirectorThread.java
+++ orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/proximity/ProximityDirectorThread.java
@@ -12,6 +12,7 @@ import java.util.concurrent.locks.LockSupport;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
