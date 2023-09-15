67f69ec0bf04028d381cb15cd5a87777a527556b
kFor <kforbro@gmail.com>
2023-09-15 23:23:27 +0300

    remove unused import

--- orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/proximity/ProximityDirectorThread.java
+++ orebfuscator-plugin/src/main/java/net/imprex/orebfuscator/proximity/ProximityDirectorThread.java
@@ -12,7 +12,6 @@ import java.util.concurrent.locks.LockSupport;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
