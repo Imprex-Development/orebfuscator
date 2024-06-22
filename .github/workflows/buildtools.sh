8051aae03c502e760b3051617a75fdb392945dc5
Maurice Eisenblätter <code@ingrim4.me>
2024-06-22 23:32:39 +0200

    ci: remove unused buildtools versions

--- .github/workflows/buildtools.sh
+++ .github/workflows/buildtools.sh
@@ -24,14 +24,6 @@ checkVersion () {
   fi
 }
 
-checkVersion "1.9.4"  "8"
-checkVersion "1.10.2" "8"
-checkVersion "1.11.2" "8"
-checkVersion "1.12.2" "8"
-checkVersion "1.13"   "8"
-checkVersion "1.13.2" "8"
-checkVersion "1.14.4" "8"
-checkVersion "1.15.2" "8"
 checkVersion "1.16.1" "8"
 checkVersion "1.16.3" "8"
 checkVersion "1.16.5" "8"
